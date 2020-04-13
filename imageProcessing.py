6067d88a18df52bf37d2525b8dae87a241307cc6
ignasandr <63236361+ignasandr@users.noreply.github.com>
2020-04-13 10:00:10 +0300

    Batch image conversion

--- imageProcessing.py
+++ imageProcessing.py
@@ -1,8 +1,16 @@
-from PIL import Image, ImageFilter
+from PIL import Image
 import sys
 import os
 
 image_folder = sys.argv[1]
 output_folder = sys.argv[2]
 
-print(image_folder, output_folder)
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+for filename in os.listdir(image_folder):
+    img = Image.open(f'{image_folder}{filename}')
+    imgRot = img.rotate(90)
+    clean_name = os.path.splitext(filename)[0]
+    imgRot.save(f'{output_folder}{clean_name}.png', 'png')
+    print('done!')
