16f6eef5f2ec07bb0a6792e8a1202377a04240c8
ignasandr <63236361+ignasandr@users.noreply.github.com>
2020-04-14 09:19:12 +0300

    Creates sorted list

--- scrape.py
+++ scrape.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
 soup = BeautifulSoup(res.text, 'html.parser')
@@ -7,17 +8,21 @@ links = soup.select('.storylink')
 subtext = soup.select('.subtext')
 
 
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['score'], reverse=True)
+
+
 def create_custom_hn(sites, points):
     hn = []
     for idx, item in enumerate(sites):
         title = sites[idx].getText()
         href = sites[idx].get('href', None)
-        vote = subtext[idx].select('.score')
+        vote = points[idx].select('.score')
         if len(vote):
             score = int(vote[0].getText().replace(' points', ''))
+            if score > 99:
+                hn.append({'title': title, 'link': href, 'score': score})
+    return sort_stories_by_votes(hn)
 
-        hn.append({'title': title, 'link': href, 'score': score})
-    return hn
 
-
-print(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(links, subtext))
