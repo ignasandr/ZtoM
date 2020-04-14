666bd05410c69edaf4f717ac944d3a01fb508029
ignasandr <63236361+ignasandr@users.noreply.github.com>
2020-04-14 10:36:00 +0300

    Two pages

--- scrape.py
+++ scrape.py
@@ -3,9 +3,16 @@ from bs4 import BeautifulSoup
 import pprint
 
 res = requests.get('https://news.ycombinator.com/news')
+res2 = requests.get('https://news.ycombinator.com/news?p=2')
 soup = BeautifulSoup(res.text, 'html.parser')
+soup2 = BeautifulSoup(res2.text, 'html.parser')
 links = soup.select('.storylink')
+links2 = soup2.select('.storylink')
 subtext = soup.select('.subtext')
+subtext2 = soup2.select('.subtext')
+
+all_links = links + links2
+all_subtext = subtext + subtext2
 
 
 def sort_stories_by_votes(hnlist):
@@ -14,6 +21,7 @@ def sort_stories_by_votes(hnlist):
 
 def create_custom_hn(sites, points):
     hn = []
+    counter = 0
     for idx, item in enumerate(sites):
         title = sites[idx].getText()
         href = sites[idx].get('href', None)
@@ -22,7 +30,9 @@ def create_custom_hn(sites, points):
             score = int(vote[0].getText().replace(' points', ''))
             if score > 99:
                 hn.append({'title': title, 'link': href, 'score': score})
+                counter += 1
+    print(counter)
     return sort_stories_by_votes(hn)
 
 
-pprint.pprint(create_custom_hn(links, subtext))
+pprint.pprint(create_custom_hn(all_links, all_subtext))
